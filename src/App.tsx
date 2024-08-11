98eda2612a5bbfccb1429082f69a95d237af7f2d
Ilya Matsiushyn <matushin.97@gmail.com>
2024-08-11 20:33:51 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import CameraCapture from './camera'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
- 
       <CameraCapture />
-  
   )
 }
 
